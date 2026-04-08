a1645bf3bcc612264303056a6830ea5173e71e55
Duy Nguyễn <nd152005vn@gmail.com>
2026-04-08 17:22:10 +0700

    now able to run frontend code

--- superset-frontend/webpack.config.js
+++ superset-frontend/webpack.config.js
@@ -391,7 +391,7 @@ const config = {
       {
         test: /\.jsx?$/,
         // include source code for plugins, but exclude node_modules and test files within them
-        exclude: [/superset-ui.*\/node_modules\//, /\.test.jsx?$/],
+        exclude: [/superset-ui.*[/\\]node_modules[/\\]/, /\.test.jsx?$/],
         include: [
           new RegExp(`${APP_DIR}/(src|.storybook|plugins|packages)`),
           ...['./src', './.storybook', './plugins', './packages'].map(p =>
